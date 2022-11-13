a2101f5b5f610d7c7d39f7d9dc78ad70b380dd0f
d1av <davi4alves@gmail.com>
2022-11-13 01:04:42 -0300

    feat: add style for the feed mobile

--- src/app/shared/components/feed/feed.component.ts
+++ src/app/shared/components/feed/feed.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from './post.type';
 
 @Component({
   selector: 'app-feed',
@@ -7,6 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedComponent implements OnInit {
 
+  public posts: Array<Post> = [
+    {
+      descricao: 'Aves são uma classe de seres vivos e penas, um bico sem dentes, oviparidade de casca rígida, elevado m. ',
+      quantidadeCurtidas: 32,
+      foto: 'https://www.petz.com.br/blog/wp-content/uploads/2022/05/passaro-tuim-interna-1.jpg',
+      usuario: {
+        nome: 'davi'
+      },
+      comentarios: [
+        {
+          nome: 'amigo',
+          comentario: 'cool'
+        },
+        {
+          nome: 'davi',
+          comentario: 'cool'
+        },
+      ]
+    } as Post,
+    {
+      descricao: 'eu não sou bomzinho',
+      quantidadeCurtidas: 32,
+      foto: 'https://www.petz.com.br/blog/wp-content/uploads/2020/01/passaros-domesticos.jpg',
+      usuario: {
+        nome: 'davi'
+      },
+      comentarios: [
+        {
+          nome: 'Loro',
+          comentario: 'Claro que é bomzinho'
+        },
+        {
+          nome: 'davi',
+          comentario: 'cool'
+        },
+      ]
+    } as Post,
+  ]
   constructor() { }
 
   ngOnInit(): void {
